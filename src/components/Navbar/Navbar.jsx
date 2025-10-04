bf762ecba47818ef43aa332adc68f162e1de9125
Hugo Machado <hugo_machado02@hotmail.com>
2025-10-04 18:29:57 -0300

    ✨ feat: Cria componente navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useRef, useEffect } from 'react'
+import './Navbar.css'
+import image from '../../assets/doctor.jpg'
+
+const Navbar = () => {
+    const [isMenuUserOpen, setIsMenuUserOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
+    const toggleMenuUser = () => {
+        setIsMenuUserOpen(!isMenuUserOpen);
+    };
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsMenuUserOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
+
+    return (
+        <div>
+            <nav className='navbar'>
+                <div className='logo'>
+                    <a href="#" className='element'>Healths<span>Sync</span></a>
+
+                </div>
+                <ul className='list'>
+                    <li><a href="#" className='element marked'>Home</a></li>
+                    <li><a href="#" className='element'>Pacientes</a></li>
+                    <li><a href="#" className='element'>Agendamentos</a></li>
+                    <li><a href="#" className='element'>Avisos</a></li>
+                </ul>
+                <div className='user' onClick={toggleMenuUser}>
+                    <div className='info'>
+                        <p className='name'>Dr. Ricardo Luz</p>
+                        <p className='specialty'>Oftamologista</p>
+                    </div>
+                    <div className='photo'>
+                        <img src={image} alt="Foto de Perfil" />
+                    </div>
+                </div>
+
+                {isMenuUserOpen && (
+                    <div className='dropdown-menu' ref={dropdownRef}>
+                        <a href="#" className='menu-item'>Minha Conta</a>
+                        <a href="#" className='menu-item'>Alterar Senha</a>
+                        <hr className='menu-divider' />
+                        <a href="#" className='menu-item logout'>Sair</a>
+                    </div>
+                )}
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
