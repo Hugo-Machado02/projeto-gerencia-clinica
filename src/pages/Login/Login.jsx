a95f4d167caaba062df4494e564cac7b449fbe6c
Hugo Machado <hugo_machado02@hotmail.com>
2025-10-05 13:55:05 -0300

    ✨ feat: Adiciona o placehoder no input

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import InputGroup from '../../Components/InputGroup/InputGroup'
+import useForm from '../../hooks/useForm';
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [formData, handleChange] = useForm({
+        emailTxt: "",
+        senhaTxt: "",
+    });
+
+
+    return (
+        <div className='container-login'>
+            <form onSubmit={handleChange} className='formulario-login'>
+                <div className="element-container-login">
+                    <InputGroup type="text" value={formData.emailTxt} name="emailTxt" onChange={handleChange} label="E-mail" placehoder="Digite o E-mail" className="element-"></InputGroup>
+                </div>
+                <div className="element-container-login">
+                    <InputGroup type="password" value={formData.senha} name="senhaTxt" onChange={handleChange} label="Senha" placehoder="Digite a Senha"  className="element-login"></InputGroup>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Login
