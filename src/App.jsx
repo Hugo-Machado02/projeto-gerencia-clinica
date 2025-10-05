4d827ad5f13b52616bbc3dbe522f9ca54b86ffba
Yan Lemke de Castro <yan.castro@compass.uol>
2025-10-05 15:23:35 -0300

    fix
    concertando os erros pleo merge anterior

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css'
-<<<<<<< HEAD
-=======
 import Paciente from './pages/Paciente/Paciente'
->>>>>>> Branch-Yan
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-=======
       <Paciente />
->>>>>>> Branch-Yan
     </>
   )
 }
