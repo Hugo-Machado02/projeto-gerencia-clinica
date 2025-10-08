e66140a6e13af6911f998eb407cdbfc6efaa67a9
Hugo Machado <hugo_machado02@hotmail.com>
2025-10-08 03:12:48 -0300

    ✨ feat: Implementa rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import Login from './pages/Login/Login'
+import Medico from './pages/Medico/Medico'
+import Paciente from './pages/Paciente/Paciente'
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "/medico",
+    element: <Medico />
+  },
+  {
+    path: "/paciente",
+    element: <Paciente />
+  },
+  {
+    path: "/development",
+    element: <App />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
